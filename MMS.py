344b9b93876f736f7872ee21dd3be2c29efb1f34
ANIKETGUP3838 <144110642+ANIKETGUP3838@users.noreply.github.com>
2025-06-02 17:51:01 +0530

    Update MMS.py

--- MMS.py
+++ MMS.py
@@ -59,7 +59,7 @@ if uploaded_file:
         df_filtered = df[(df['Gender'].isin(selected_gender)) & (df['Age'].between(*selected_age))]
 
         st.subheader("Dataset Preview")
-        st.dataframe(data.head())
+        st.dataframe(df.head())
         
         st.subheader("1. Demographic & Billing Analysis")
         col1, col2 = st.columns(2)
